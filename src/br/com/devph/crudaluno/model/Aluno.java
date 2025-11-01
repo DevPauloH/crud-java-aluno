8983c0872285a79a4b3cca8cb9dc31c5ca1619ca
DevPauloH <henriquedsg15@gmail.com>
2025-10-31 22:50:22 -0300

    Criando entidade(Classe) Aluno

--- src/br/com/devph/crudaluno/model/Aluno.java
+++ src/br/com/devph/crudaluno/model/Aluno.java
@@ -1,5 +1,48 @@
 package br.com.devph.crudaluno.model;
 
-public class Aluno {
+    public class Aluno {
+        private int id;
+        private String nome;
+        private int idade;
+        private String email;
 
-}
+        public Aluno(){}
+
+        public Aluno(String nome, int idade, String email){
+            this.nome = nome;
+            this.idade = idade;
+            this.email = email;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public int getIdade() {
+            return idade;
+        }
+
+        public void setIdade(int idade) {
+            this.idade = idade;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
