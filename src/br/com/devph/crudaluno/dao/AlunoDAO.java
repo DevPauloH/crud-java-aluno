6a2d3190c69ac0dabe16b3abdc5a0e806d30e042
DevPauloH <henriquedsg15@gmail.com>
2025-11-02 21:43:15 -0300

    Criando listagem de alunos

--- src/br/com/devph/crudaluno/dao/AlunoDAO.java
+++ src/br/com/devph/crudaluno/dao/AlunoDAO.java
@@ -9,11 +9,11 @@ import java.util.List;
 
 public class AlunoDAO {
 
-    public void adicionar(Aluno aluno){
+    public void adicionar(Aluno aluno) {
         String sql = "INSERT INTO aluno(nome, idade, email) VALUES(?, ?, ?)";
 
-        try(Connection conn = conexao.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(sql)){
+        try (Connection conn = conexao.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setString(1, aluno.getNome());
             stmt.setInt(2, aluno.getIdade());
@@ -24,6 +24,33 @@ public class AlunoDAO {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
+    }
+
+
+
+    public List<Aluno> listagemAlunos(){
+        List<Aluno> lista = new ArrayList<>();
+        String sql = "SELECT * FROM aluno";
+
+        try(Connection conn = conexao.getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)){
+            while(rs.next()){
+                Aluno a = new Aluno();
+                a.setId(rs.getInt("id"));
+                a.setNome(rs.getString("nome"));
+                a.setIdade(rs.getInt("idade"));
+                a.setEmail(rs.getString("email"));
+                lista.add(a);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+            return lista;
+        }
+
 
     }
 }
